fd236da17daf37e28d92fd2dc89d7d2271046a24
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-07-12 09:48:33 +0200

    Add JWT security configuration

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use App\Enum\UserRole;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -17,7 +19,10 @@ class User
     private string $email;
 
     #[ORM\Column(type: 'string')]
-    private string $passwordHash;
+    private string $password;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
 
     #[ORM\Column(enumType: UserRole::class)]
     private UserRole $role;
@@ -53,15 +58,39 @@ class User
         return $this;
     }
 
-    public function getPasswordHash(): string
+    public function getPassword(): string
     {
-        return $this->passwordHash;
+        return $this->password;
     }
 
-    public function setPasswordHash(string $passwordHash): self
+    public function setPassword(string $password): self
     {
-        $this->passwordHash = $passwordHash;
+        $this->password = $password;
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
     }
 
     public function getRole(): UserRole

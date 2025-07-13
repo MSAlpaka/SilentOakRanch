2ead8a89241be068f13a0cef9f9a834245697753
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-07-13 16:18:48 +0200

    Add monthly and per use pricing units

--- backend/src/Enum/PricingUnit.php
+++ backend/src/Enum/PricingUnit.php
@@ -7,6 +7,8 @@ enum PricingUnit: string
     case PER_BOOKING = 'per_booking';
     case PER_DAY = 'per_day';
     case PER_HOUR = 'per_hour';
+    case PER_MONTH = 'per_month';
+    case PER_USE = 'per_use';
     case FLAT = 'flat';
 }
 

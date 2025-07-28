7a30f6f74e6c67cb2dcc8a19479ff1f30b1b6929
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-07-28 19:39:36 +0200

    Prevent duplicate scale bookings

--- backend/src/Controller/HorseScale/BookingFormController.php
+++ backend/src/Controller/HorseScale/BookingFormController.php
@@ -6,17 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ScaleBookingRepository;
 
 class BookingFormController extends AbstractController
 {
     #[Route('/horsescale/book', name: 'horsescale_booking_form', methods: ['GET', 'POST'])]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, ScaleBookingRepository $repository): Response
     {
         if ($request->isMethod('POST')) {
-            // Here one would normally persist the booking request
-            $this->addFlash('success', 'Booking request received.');
+            $date = $request->request->get('date');
+            $name = $request->request->get('name');
 
-            return $this->redirectToRoute('horsescale_booking_form');
+            if ($date && $name) {
+                try {
+                    $bookingDateTime = new \DateTimeImmutable($date);
+                } catch (\Exception) {
+                    $this->addFlash('error', 'Invalid date.');
+                    return $this->redirectToRoute('horsescale_booking_form');
+                }
+
+                if ($repository->existsForDateTime($bookingDateTime)) {
+                    $this->addFlash('error', 'Selected time slot is already booked.');
+                } else {
+                    // Here one would normally persist the booking request
+                    $this->addFlash('success', 'Booking request received.');
+                }
+
+                return $this->redirectToRoute('horsescale_booking_form');
+            }
         }
 
         return $this->render('horsescale/booking_form.html.twig');

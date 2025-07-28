d5821fdb4203b77bcd1946932b57cfae143fdead
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-07-28 19:13:00 +0200

    Add PDF and QR code services

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];

244593654ed07e114f1aaf1b3cb928dab0296685
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-09-12 18:14:46 +0200

    Add appointment email methods and templates

--- backend/src/Service/MailService.php
+++ backend/src/Service/MailService.php
@@ -8,11 +8,95 @@ use App\Entity\User;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MailService
 {
-    public function __construct(private readonly MailerInterface $mailer)
-    {
+    public function __construct(
+        private readonly MailerInterface $mailer,
+        private readonly TranslatorInterface $translator
+    ) {
+    }
+
+    public function sendAppointmentConfirmation(
+        string $email,
+        string $userName,
+        string $horseName,
+        string $serviceType,
+        string $date,
+        string $time,
+        string $providerName,
+        string $stableName
+    ): void {
+        $message = (new TemplatedEmail())
+            ->to(new Address($email))
+            ->subject($this->translator->trans('appointment.confirmation.subject'))
+            ->htmlTemplate('emails/appointments/confirmation.html.twig')
+            ->context([
+                'userName' => $userName,
+                'horseName' => $horseName,
+                'serviceType' => $serviceType,
+                'date' => $date,
+                'time' => $time,
+                'providerName' => $providerName,
+                'stableName' => $stableName,
+            ]);
+
+        $this->mailer->send($message);
+    }
+
+    public function sendAppointmentReminder(
+        string $email,
+        string $userName,
+        string $horseName,
+        string $serviceType,
+        string $date,
+        string $time,
+        string $providerName,
+        string $stableName
+    ): void {
+        $message = (new TemplatedEmail())
+            ->to(new Address($email))
+            ->subject($this->translator->trans('appointment.reminder.subject'))
+            ->htmlTemplate('emails/appointments/reminder.html.twig')
+            ->context([
+                'userName' => $userName,
+                'horseName' => $horseName,
+                'serviceType' => $serviceType,
+                'date' => $date,
+                'time' => $time,
+                'providerName' => $providerName,
+                'stableName' => $stableName,
+            ]);
+
+        $this->mailer->send($message);
+    }
+
+    public function sendAppointmentCancellation(
+        string $email,
+        string $userName,
+        string $horseName,
+        string $serviceType,
+        string $date,
+        string $time,
+        string $providerName,
+        string $stableName
+    ): void {
+        $message = (new TemplatedEmail())
+            ->to(new Address($email))
+            ->subject($this->translator->trans('appointment.cancellation.subject'))
+            ->htmlTemplate('emails/appointments/cancellation.html.twig')
+            ->context([
+                'userName' => $userName,
+                'horseName' => $horseName,
+                'serviceType' => $serviceType,
+                'date' => $date,
+                'time' => $time,
+                'providerName' => $providerName,
+                'stableName' => $stableName,
+            ]);
+
+        $this->mailer->send($message);
     }
 
     public function sendInvitation(string $email, string $token): void
